3ce287afc2094b7d8e8be192a17c77c1c7a3595a
Baimini <98502673+Baimini@users.noreply.github.com>
2024-09-25 22:55:16 +0800

    feat: Setting omni wheels motor pinout
    Complete the setup of 8 pins.
    
    issue #8

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,20 @@
 #include <Arduino.h>
 
+#define FRONT_IN1   18
+#define FRONT_IN2   2
+
+#define FRONT_IN3   4
+#define FRONT_IN4   16
+
+#define BACK_IN1    13
+#define BACK_IN2    25
+
+#define BACK_IN3    27
+#define BACK_IN4    26
+
 void setup() {
     Serial.begin(115200);
+    Serial.printf("\n");
     while(!Serial);
 }
 
