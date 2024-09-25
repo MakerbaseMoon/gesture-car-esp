d2e093f8393b9028a5845bd47bb4007297350598
Baimini <98502673+Baimini@users.noreply.github.com>
2024-09-25 23:01:10 +0800

    feat: Configure web server on ESP32
    Set up a basic web server on ESP32 using
    PlatformIO.
    ESP32 successfully integrated with front-end html.
    
    issue #9

--- src/main.cpp
+++ src/main.cpp
@@ -15,10 +15,63 @@
 #define BACK_IN3    27
 #define BACK_IN4    26
 
+const char* ssid = SSID;
+const char* password = PASSWORD;
+
+AsyncWebServer server(80);
+
 void setup() {
     Serial.begin(115200);
     Serial.printf("\n");
     while(!Serial);
+
+    if(!SPIFFS.begin(true)) {
+        Serial.println("SPIFFS initialization failed!");
+        return;
+    }
+    Serial.println("SPIFFS initialization successful!");
+
+    WiFi.begin(ssid, password);
+    while(WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("\nConnected to Wi-Fi");
+
+    Serial.print("ESP32 IP: ");
+    Serial.println(WiFi.localIP());
+
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+        Serial.println("server success");
+        request->send(SPIFFS, "/index.html", "text/html");
+    });
+
+    server.on("/main.js", HTTP_GET, [](AsyncWebServerRequest *request){
+        Serial.println("javascript success");
+        request->send(SPIFFS, "/main.js", "application/javascript");
+    });
+
+    server.on("/forward", HTTP_GET, [](AsyncWebServerRequest *request){
+        Serial.println("forward");
+        request->send(200, "text/plain", "Forward command received");
+    });
+
+    server.on("/backward", HTTP_GET, [](AsyncWebServerRequest *request){
+        Serial.println("backward");
+        request->send(200, "text/plain", "Backward command received");
+    });
+
+    server.on("/left", HTTP_GET, [](AsyncWebServerRequest *request){
+        Serial.println("left");
+        request->send(200, "text/plain", "Left command received");
+    });
+
+    server.on("/right", HTTP_GET, [](AsyncWebServerRequest *request){
+        Serial.println("right");
+        request->send(200, "text/plain", "Right command received");
+    });
+
+    server.begin();
 }
 
 void loop() {
